20d89aed0b513eebad1615d7129b78b454c3fba0
at//design <mail@at-world.de>
2024-03-12 12:52:47 +0100

    Cleanup
    - removed empty test

--- Tests/GADesignSystemTests/GADesignSystemTests.swift
+++ Tests/GADesignSystemTests/GADesignSystemTests.swift
@@ -3,12 +3,4 @@ import XCTest
 import GADesignSystem
 
 final class GADesignSystemTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-      let _ = GSDS.Colors.Text.Primary.inverse
-    }
 }
